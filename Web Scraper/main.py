17ecfac0a0ab30e72670361bfe9419498217a96e
0937452 <0937452@hr.nl>
2017-09-22 17:01:46 +0200

    .

--- Web Scraper/main.py
+++ Web Scraper/main.py
@@ -1,6 +1,9 @@
 import Laptop
 import Desktop
+import Monitor
 import Printer
+import Mouse
+import Keyboard
 
 import time
 from urllib.request import urlopen
@@ -12,7 +15,7 @@ def main():
 
     start_time = time.clock()
 
-    products = [Laptop.Laptop(), Desktop.Desktop(), Printer.Printer()]
+    products = [Laptop.Laptop(), Desktop.Desktop(), Monitor.Monitor(), Printer.Printer(), Mouse.Mouse(), Keyboard.Keyboard()]
 
     for product in products:
         product.run()
