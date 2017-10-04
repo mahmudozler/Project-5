64f6f3d15d0c759627095cec41a34cca03ca6b11
Satrya Sabeni <s.sabeni17@gmail.com>
2017-10-04 13:57:07 +0200

    javascript link

---
+++ Html/js/script.js
@@ -0,0 +1,12 @@
+
+    function zre(value){
+
+
+  if(value == "s8"){
+  document.getElementById("zrtext").innerHTML="Resulaten"
+  document.getElementById("zr").src="img/iphonex.png"
+  document.getElementById("zr2").src="img/iphonex.png"
+  document.getElementById("zr3").src="img/iphonex.png"
+  document.getElementById("zr4").src="img/iphonex.png"
+  }
+}
