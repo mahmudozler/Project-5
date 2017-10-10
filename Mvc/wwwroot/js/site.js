10bd3a82d5bd5a9ef041f04481f3eea42f95b8cf
Modmr <mo_42@hotmail.nl>
2017-10-10 17:30:00 +0200

    Zoekfunctie javascript gedeelte
    niks is waar alles is gelogen

--- Mvc/wwwroot/js/site.js
+++ Mvc/wwwroot/js/site.js
@@ -1,1 +1,8 @@
-﻿// Write your JavaScript code.
+﻿function zre(value,e)
+{
+    if(e.keyCode === 13){
+        e.preventDefault();
+    window.location.replace("http://localhost:5000/Home/ZoekResultaten");
+    }
+      
+}// Write your JavaScript code.
