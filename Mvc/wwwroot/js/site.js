7e5d4b91d2f40e71da7f52300b11a2bb006593ce
0937452 <0937452@hr.nl>
2017-10-10 17:51:45 +0200

    .

--- Mvc/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿function zre(value,e)
-{
-    if(e.keyCode === 13){
-        e.preventDefault();
-    window.location.replace("http://localhost:5000/Home/ZoekResultaten");
-    }
-      
-}// Write your JavaScript code.
