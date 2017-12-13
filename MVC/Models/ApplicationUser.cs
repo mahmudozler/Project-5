6fa353b5361a83fc1656dd28b57c1d20a865317a
Satrya <s.sabeni17@gmail.com>
2017-12-13 23:39:04 +0100

    smallcheck

--- MVC/Models/ApplicationUser.cs
+++ MVC/Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -12,7 +13,13 @@ namespace MVC.Models
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Address { get; set; }
+
+        [RegularExpression("[0-9]{4}"+"[A-Z]{2}",ErrorMessage = "Vul een correct postcode in(bijv. 1234AB)")]
         public string Zipcode { get; set; }
+
+        [RegularExpression("[0-9]{10}",ErrorMessage = "Vul een 10 cijferife nummer in(bijv. 0612345678)")]
+        public override string PhoneNumber { get; set; }
+
 
     }
 }
--- MVC/Models/ManageViewModels/IndexViewModel.cs
+++ MVC/Models/ManageViewModels/IndexViewModel.cs
@@ -19,10 +19,12 @@ namespace MVC.Models.ManageViewModels
         [Display(Name = "Address")]
         public string Address { get; set; }
 
+        [StringLength(6, MinimumLength = 6),RegularExpression("[0-9]{4}"+"[A-Z]{2}",ErrorMessage = "Vul een correct postcode in(bijv. 1234AB)")]
         [Display(Name = "Postcode")]
         public string Zipcode { get; set; }
 
         [Phone]
+        [RegularExpression("[0-9]{10}",ErrorMessage = "Vul een 10 cijferife nummer in(bijv. 0612345678)")]
         [Display(Name = "telefoon nummer")]
         public string PhoneNumber { get; set; }
 
