7c21c9e389bc4aa594aa9b79f3993a7531f75d5b
Endy Hu <0937452@hr.nl>
2017-12-19 13:59:34 +0100

    .

--- MVC/Controllers/ProductController.cs
+++ MVC/Controllers/ProductController.cs
@@ -145,7 +145,7 @@ namespace MVC.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("id,type,name,description,price,amount")] Product product)
+        public async Task<IActionResult> Create([Bind("Id,Type,Name,Description,Price,Amount,Sold")] Product product)
         {
             if (ModelState.IsValid)
             {
@@ -177,7 +177,7 @@ namespace MVC.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("id,type,name,description,price,amount,sold")] Product product)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Type,Name,Description,Price,Amount,Sold")] Product product)
         {
             if (id != product.Id)
             {
