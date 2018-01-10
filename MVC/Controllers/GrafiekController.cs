046424c3f6af70c6a57def3791a42357f670c7f0
Modmr <mo_42@hotmail.nl>
2018-01-10 18:11:46 +0100

    grafieken

---
+++ MVC/Controllers/GrafiekController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace MVC.Controllers
+{
+    public class GrafiekController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
