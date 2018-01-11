0610509e2cd886d2f9cd09e0add5b86cdff247c0
Endy Hu <0937452@hr.nl>
2018-01-11 20:46:15 +0100

    .

--- MVC/Controllers/GrafiekController.cs
+++ MVC/Controllers/GrafiekController.cs
@@ -6,14 +6,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
-
+using MVC.Models;
+using MVC.Data;
 
 namespace MVC.Controllers
 {
     public class GrafiekController : Controller
     {
+        ProductDbContext _context;
+        public GrafiekController(ProductDbContext context){
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var partialOrders = _context.PartialOrder.GroupBy(g => g.OrderId).Select(s => s.First());
+
+            ViewBag.Data = partialOrders.ToArray();
+
             return View();
         }
     }
