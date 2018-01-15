e2f6aceae5c1ed31d2d616e17ab92110a2a85461
Modmr <mo_42@hotmail.nl>
2018-01-15 17:54:22 +0100

    grafiek

--- MVC/Controllers/GrafiekController.cs
+++ MVC/Controllers/GrafiekController.cs
@@ -1,30 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Tasks; 
+using Microsoft.AspNetCore.Mvc; 
+using Microsoft.AspNetCore.Mvc.Rendering; 
+using Microsoft.EntityFrameworkCore; 
 
-using MVC.Models;
-using MVC.Data;
+using MVC.Models; 
+using MVC.Data; 
 
 namespace MVC.Controllers
 {
-    public class GrafiekController : Controller
+    public class GrafiekController:Controller
     {
-        ProductDbContext _context;
-        public GrafiekController(ProductDbContext context){
-            _context = context;
+        ProductDbContext _context; 
+        public GrafiekController(ProductDbContext context)
+        {
+            _context = context; 
         }
 
         public IActionResult Index()
         {
-            var partialOrders = _context.PartialOrder.GroupBy(g => g.OrderId).Select(s => s.First()).OrderByDescending(o => o.Date);
+            var partialOrders = _context.PartialOrder.GroupBy(g => g.OrderId).Select(s => s.First()).OrderByDescending(o => o.Date); 
 
-            ViewBag.Data = partialOrders.ToArray();
+            ViewBag.Data = partialOrders.ToArray(); 
+            
+            var product_ordered = _context.Products.OrderByDescending(p => p.Sold).ToList();
+            List<Product> best_sold = new List<Product>();
 
-            return View();
+            for (int i = 0; i < 10; i++)
+            {
+                best_sold.Add(product_ordered[i]);
+            }
+
+            return View(best_sold); 
         }
+
+        
     }
 }
